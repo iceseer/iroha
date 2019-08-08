addc25e573bf9e52abcd04fd75f0eef3ece2043f
Andrei Lebedev <lebdron@gmail.com>
2019-08-08 20:33:38 +0300

    Pass PostgresSpecificQueryExecutor to PostgresCommandExecutor
    Use executors in EngineCall
    
    Signed-off-by: Andrei Lebedev <lebdron@gmail.com>
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>
    Signed-off-by: Eugene Kovalev <eugene.a.kovalev@gmail.com>
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>
    
    # Conflicts:
    #	test/module/irohad/ametsuchi/postgres_executor_test.cpp

--- test/module/irohad/ametsuchi/postgres_query_executor_test.cpp
+++ test/module/irohad/ametsuchi/postgres_query_executor_test.cpp
@@ -20,6 +20,7 @@
 #include <boost/range/size.hpp>
 #include "ametsuchi/impl/flat_file/flat_file.hpp"
 #include "ametsuchi/impl/postgres_command_executor.hpp"
+#include "ametsuchi/impl/postgres_specific_query_executor.hpp"
 #include "ametsuchi/impl/postgres_wsv_query.hpp"
 #include "ametsuchi/mutable_storage.hpp"
 #include "backend/plain/peer.hpp"
@@ -160,8 +161,16 @@ namespace iroha {
         executor = std::make_unique<PostgresCommandExecutor>(
             std::make_unique<soci::session>(*soci::factory_postgresql(),
                                             pgopt_),
-            perm_converter);
-        pending_txs_storage = std::make_shared<MockPendingTransactionStorage>();
+            perm_converter,
+            std::make_shared<PostgresSpecificQueryExecutor>(
+                *sql,
+                *block_storage_,
+                pending_txs_storage,
+                query_response_factory,
+                perm_converter,
+                getTestLoggerManager()
+                    ->getChild("SpecificQueryExecutor")
+                    ->getLogger()));
 
         auto query_executor_result = storage->createQueryExecutor(
             pending_txs_storage, query_response_factory);
