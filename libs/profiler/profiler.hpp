ec95a5f94e97e9e8553ee058ee102aa7ad98010a
iceseer <iceseer@gmail.com>
2020-02-21 11:27:44 +0300

    profiler
    Signed-off-by: iceseer <iceseer@gmail.com>

---
+++ libs/profiler/profiler.hpp
@@ -0,0 +1,19 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef IROHA_PROFILER_HPP
+#define IROHA_PROFILER_HPP
+
+namespace iroha { namespace performance_tools {
+
+struct {
+    // функция
+    // метка
+    // такты
+    // частота
+};
+
+}}
+
+#endif//IROHA_PROFILER_HPP
