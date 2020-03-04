e93688034e3baeec3ebe06352085545eecaca9c2
iceseer <iceseer@gmail.com>
2020-03-04 10:48:48 +0300

    1
    Signed-off-by: iceseer <iceseer@gmail.com>

--- libs/profiler/profiler.hpp
+++ libs/profiler/profiler.hpp
@@ -14,6 +14,30 @@ struct {
     // частота
 };
 
+
+struct Profiler final {
+    using ProfilerHandle = uint64_t;
+    using Hash = uint64_t;
+    using PerformanceCounter = uint64_t;
+
+    Profiler(Profiler const&) = delete;
+    Profiler& operator=(Profiler const&) = delete;
+
+    Profiler(Profiler&&) = delete;
+    Profiler& operator=(Profiler&&) = delete;
+
+    Profiler() = default;
+    ~Profiler() = default;
+
+public:
+    inline void push(Hash hash, char const *tag);
+    inline void pop(PerformanceCounter counter);
+};
+
+
+extern ProfilerHandle getProfilerHandle();
+
+
 }}
 
 #endif//IROHA_PROFILER_HPP
