3fb8fe8f40923de388df6cc47c6b7e575846c3e7
Mikhail Boldyrev <miboldyrev@gmail.com>
2020-05-02 11:59:30 -0500

    fixup! added CallEngine command
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>

--- test/module/shared_model/validators/field_validator_test.cpp
+++ test/module/shared_model/validators/field_validator_test.cpp
@@ -150,11 +150,6 @@ class FieldValidatorTest : public ValidatorsTest {
 
     // TODO(Ivan Tyulyandin): add test cases for smart contract parameters
 
-    field_validators.insert(makeValidator("callee",
-                                          &FieldValidator::validateCallee,
-                                          &FieldValidatorTest::callee,
-                                          {}));
-
     field_validators.insert(makeValidator("input",
                                           &FieldValidator::validateBytecode,
                                           &FieldValidatorTest::input,
@@ -734,12 +729,11 @@ class FieldValidatorTest : public ValidatorsTest {
           &FieldValidatorTest::amount,
           [](auto &&x) { return shared_model::interface::Amount(x); },
           amount_test_cases),
-      makeTransformValidator(
-          "peer",
-          &FieldValidator::validatePeer,
-          &FieldValidatorTest::peer,
-          [](auto &&x) { return proto::Peer(x); },
-          peer_test_cases),
+      makeTransformValidator("peer",
+                             &FieldValidator::validatePeer,
+                             &FieldValidatorTest::peer,
+                             [](auto &&x) { return proto::Peer(x); },
+                             peer_test_cases),
       makeValidator("account_name",
                     &FieldValidator::validateAccountName,
                     &FieldValidatorTest::account_name,
@@ -874,7 +868,7 @@ TEST_F(FieldValidatorTest, QueryContainerFieldsValidation) {
         // Skip oneof types
         if (field->containing_oneof()
             == iroha::protocol::Query::Payload::descriptor()->FindOneofByName(
-                "query")) {
+                   "query")) {
           return;
         }
         this->runTestCases(field);
